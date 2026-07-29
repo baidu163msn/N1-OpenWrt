af5adc8e57576bf5329529feaec62b73f2783df3
旅行者叄号 <79754051+baidu163msn@users.noreply.github.com>
2026-07-29 08:23:54 +0800

    更新 diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -5,3 +5,7 @@ sed -i 's/ImmortalWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
 
 # Set default root password to "password"
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/base-files/files/etc/shadow
+
+# Add sing-box feed (if not in default feeds)
+# Option A: from small_5 feed (common source)
+git clone https://github.com/small-5/openwrt-sing-box package/sing-box
