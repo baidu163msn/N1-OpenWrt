9f518c5c17d113e27faf97c0cbdc138749c7c197
旅行者叄号 <79754051+baidu163msn@users.noreply.github.com>
2026-07-30 12:23:46 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -6,5 +6,8 @@ sed -i 's/ImmortalWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
 # Set default root password to "password"
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/base-files/files/etc/shadow
 
-# Add luci-app-amlogic (ophub, for N1 eMMC install/upgrade)
+# Add luci-app-amlogic (ophub)
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+
+# Add luci-app-openclash (vernesong)
+git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
