be95d8bb710274301ff5944b5a8adb1c6a495c8f
旅行者叄号 <79754051+baidu163msn@users.noreply.github.com>
2026-07-30 09:01:32 +0800

    更新 diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# Set default hostname
 sed -i 's/ImmortalWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/base-files/files/etc/shadow
 
-# Add independent sing-box (not dependent on passwall)
-git clone --depth=1 https://github.com/kiddin9/openwrt-packages package/kiddin9
+# Set default root password to "password"
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/base-files/files/etc/shadow
